34a55034a5ceb27509cc7bba6fe165c8d9d2f76a
winmychance <120444160+winmychance@users.noreply.github.com>
2022-01-02 15:54:42 +0000

    closes #20

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -3,4 +3,4 @@ use rusqlite::{Connection, params};
 use walkdir::WalkDir;
 
 fn main() {
-    let current_dir = std::env::current_dir().expect(
+    let current_dir = std::env::current_dir().expect("Current Directo
