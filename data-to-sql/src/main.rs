cee81ab77ab6a0f450e6a48809eedf718ab5bc4d
winmychance <120444160+winmychance@users.noreply.github.com>
2022-01-09 20:00:08 +0000

    [l10n] Update Japanese translation

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -4,4 +4,4 @@ use walkdir::WalkDir;
 
 fn main() {
     let current_dir = std::env::current_dir().expect("Current Directory Exists");
-    let parent_path_obj = current_dir.parent().expect("P
+    let parent_path_obj = current_dir.parent().expect("Parent exists
