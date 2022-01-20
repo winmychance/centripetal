d5064baf254894efdfdabdfcb2978a02712d892f
winmychance <120444160+winmychance@users.noreply.github.com>
2022-01-20 19:14:06 +0000

    Create README.org

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -5,4 +5,4 @@ use walkdir::WalkDir;
 fn main() {
     let current_dir = std::env::current_dir().expect("Current Directory Exists");
     let parent_path_obj = current_dir.parent().expect("Parent exists").join("5-min-stocks/data/5_min/us/");
-    let parent_path = parent_
+    let parent_path = parent_path_obj.to_str()
