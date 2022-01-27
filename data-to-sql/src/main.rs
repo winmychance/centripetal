f8dc0c5cb801fdbad9bfd12b281860a021ccbe56
winmychance <120444160+winmychance@users.noreply.github.com>
2022-01-27 15:36:21 +0000

    bump to 0.1.5

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -5,4 +5,6 @@ use walkdir::WalkDir;
 fn main() {
     let current_dir = std::env::current_dir().expect("Current Directory Exists");
     let parent_path_obj = current_dir.parent().expect("Parent exists").join("5-min-stocks/data/5_min/us/");
-    let parent_path = parent_path_obj.to_str().expect("Directory 
+    let parent_path = parent_path_obj.to_str().expect("Directory exists");
+
+    let 
