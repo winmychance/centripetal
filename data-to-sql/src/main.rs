26a8c313401ce3b1a20a0e2c9e00411fbf7ed666
winmychance <120444160+winmychance@users.noreply.github.com>
2022-02-14 16:50:56 +0000

    updating bash

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -7,4 +7,4 @@ fn main() {
     let parent_path_obj = current_dir.parent().expect("Parent exists").join("5-min-stocks/data/5_min/us/");
     let parent_path = parent_path_obj.to_str().expect("Directory exists");
 
-    let 
+    let conn = Connection::open("5_min
