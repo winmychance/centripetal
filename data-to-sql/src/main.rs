346e03036ae88712105083ce357575b878e3a602
winmychance <120444160+winmychance@users.noreply.github.com>
2022-03-18 06:50:39 +0000

    comment typo

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -10,4 +10,4 @@ fn main() {
     let conn = Connection::open("5_minute.db.sqlite").expect("Connection");
     conn.execute_batch(
         "PRAGMA journal_mode = OFF;
-              PRAGMA syn
+              PRAGMA synchronous = 
