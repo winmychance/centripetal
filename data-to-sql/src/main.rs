7c867036c75f9f6605b3d9f1b494572e2fee2109
winmychance <120444160+winmychance@users.noreply.github.com>
2022-04-07 16:48:39 +0000

    Update editor.html

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -12,4 +12,5 @@ fn main() {
         "PRAGMA journal_mode = OFF;
               PRAGMA synchronous = 0;
               PRAGMA cache_size = 1000000;
-              PRAGMA locking_mode = EXC
+              PRAGMA locking_mode = EXCLUSIVE;
+   
