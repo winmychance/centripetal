d5ba48f6a34cc92025dad974e69edac6d6df4035
winmychance <120444160+winmychance@users.noreply.github.com>
2022-04-29 16:57:34 +0000

    Update Rust

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -17,4 +17,4 @@ fn main() {
     )
     .expect("PRAGMA");
     let mut stmt = conn.prepare(
-        "INSERT INTO raw_data (ticker, per, date, time, open, high, low, cl
+        "INSERT INTO raw_data (ticker, per, date, time, open, high, low, close) values (?
