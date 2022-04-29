91ce041d6d6a77da2d08351823210c938762131c
winmychance <120444160+winmychance@users.noreply.github.com>
2022-04-29 19:40:45 +0000

    lkdf

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -17,4 +17,4 @@ fn main() {
     )
     .expect("PRAGMA");
     let mut stmt = conn.prepare(
-        "INSERT INTO raw_data (ticker, per, date, time, open, high, low, close) values (?
+        "INSERT INTO raw_data (ticker, per, date, time, open, high, low, close) values (?1, ?2, ?3 , ?
