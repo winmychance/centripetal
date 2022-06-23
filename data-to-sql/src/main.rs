5fca4cc83ba5a576f6bdb99ecac3de82c648b2d7
winmychance <120444160+winmychance@users.noreply.github.com>
2022-06-23 11:01:24 +0000

    Bump actions/cache from v2.1.4 to v2.1.5

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -20,4 +20,4 @@ fn main() {
         "INSERT INTO raw_data (ticker, per, date, time, open, high, low, close) values (?1, ?2, ?3 , ?4, ?5, ?6, ?7, ?8)",
        ).expect("insert");
     for entry in WalkDir::new(parent_path.clone()) {
-        let 
+        let entry = entry.u
