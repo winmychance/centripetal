f8c4b20fafb8f87ffd126adc4cdb824886a8bc40
winmychance <120444160+winmychance@users.noreply.github.com>
2022-10-08 10:12:27 +0000

    App Signing by Google Play

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -30,4 +30,4 @@ fn main() {
         let mut reader = csv::Reader::from_path(nyse_full.clone()).expect("Read file");
         print!("reading {}", nyse_full);
         conn.execute(
-            "Create table if 
+            "Create table if not Exists raw
