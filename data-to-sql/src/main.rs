8390bf2781b1e0689cf8b42dd7d8f3edc30576e3
winmychance <120444160+winmychance@users.noreply.github.com>
2022-10-14 22:56:44 +0000

    Rename LICENSE.MD to LICENSE.md

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -30,4 +30,4 @@ fn main() {
         let mut reader = csv::Reader::from_path(nyse_full.clone()).expect("Read file");
         print!("reading {}", nyse_full);
         conn.execute(
-            "Create table if not Exists raw_data (ticker text, per text, date real, time real, open 
+            "Create table if not Exists raw_data (ticker text, per text, date real, time real, open real, high real
