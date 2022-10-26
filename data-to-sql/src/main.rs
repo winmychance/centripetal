85a4cf33d17aaa284e04ced429c3a3714c2f8eb5
winmychance <120444160+winmychance@users.noreply.github.com>
2022-10-26 11:00:29 +0000

    bump version to 0.0.4

--- data-to-sql/src/main.rs
+++ data-to-sql/src/main.rs
@@ -32,4 +32,5 @@ fn main() {
         conn.execute(
             "Create table if not Exists raw_data (ticker text, per text, date real, time real, open real, high real, low real, close real)",
             [],
-        ).expect("Table created
+        ).expect("Table created");
+        pr
